faf9dd6dd6927a139128467054cbbf21d9d0c2b8
Annan Wang <awang0523@gmail.com>
2018-04-06 18:59:25 -0700

    add enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+
+[RequireComponent (typeof(UnityEngine.AI.NavMeshAgent))]
+public class Enemy : MonoBehaviour {
+
+    UnityEngine.AI.NavMeshAgent pathfinder;
+    Transform target;
+
+	void Start () {
+        pathfinder = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	
+	void Update () {
+        pathfinder.SetDestination(target.position);
+	}
+}
