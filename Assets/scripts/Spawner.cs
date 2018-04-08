725f49f21c2737003a705bc2a7393619f5346b2b
WenqiWang <ww82@sjsu.edu>
2018-04-08 02:37:00 -0700

    implement Spawner class to manage waves of enemies

--- Assets/scripts/Spawner.cs
+++ Assets/scripts/Spawner.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 public class Spawner : MonoBehaviour {
 
 	public Wave[] waves;
+	public Enemy enemy;
+
+	int enemiesRemainingToSpawn;
+	float nextSpawnTime;
+
+	void Update() {
+	}
 
 	[System.Serializable]
 	public class Wave {
