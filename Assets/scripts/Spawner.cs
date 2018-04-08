7a705d2b6fe16a0c1f556cf09dc9462076ab1191
WenqiWang <ww82@sjsu.edu>
2018-04-08 02:53:04 -0700

    first wave working

--- Assets/scripts/Spawner.cs
+++ Assets/scripts/Spawner.cs
@@ -6,10 +6,30 @@ public class Spawner : MonoBehaviour {
 	public Wave[] waves;
 	public Enemy enemy;
 
+	Wave currentWave;
+	int currentWaveNumber;
+
 	int enemiesRemainingToSpawn;
 	float nextSpawnTime;
 
+	void Start() {
+		NextWave ();
+	}
+
 	void Update() {
+		if (enemiesRemainingToSpawn > 0 && Time.time > nextSpawnTime) {
+			enemiesRemainingToSpawn--;
+			nextSpawnTime = Time.time + currentWave.timeBetweenSpawns;
+
+			Enemy spawnedEnemy = Instantiate (enemy, Vector3.zero, Quaternion.identity) as Enemy;
+		}
+	}
+
+	void NextWave() {
+		currentWaveNumber++;
+		currentWave = waves [currentWaveNumber - 1];
+
+		enemiesRemainingToSpawn = currentWave.enemyCount;
 	}
 
 	[System.Serializable]
