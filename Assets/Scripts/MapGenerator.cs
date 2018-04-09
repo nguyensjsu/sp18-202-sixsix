fba1928f7bdde5cbb51d91a5315be2dac0261473
WenqiWang <ww82@sjsu.edu>
2018-04-09 15:47:49 -0700

    implement mesh floor for all sides

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -83,6 +83,18 @@ public class MapGenerator : MonoBehaviour {
 		maskLeft.parent = mapHolder;
 		maskLeft.localScale = new Vector3 ((maxMapSize.x - mapSize.x) / 2, 1, mapSize.y) * tileSize;
 
+		Transform maskRight = Instantiate (navmeshMaskPrefab, Vector3.right * (mapSize.x + maxMapSize.x) / 4 * tileSize, Quaternion.identity) as Transform;
+		maskRight.parent = mapHolder;
+		maskRight.localScale = new Vector3 ((maxMapSize.x - mapSize.x) / 2, 1, mapSize.y) * tileSize;
+
+		Transform maskTop = Instantiate (navmeshMaskPrefab, Vector3.forward * (mapSize.y + maxMapSize.y) / 4 * tileSize, Quaternion.identity) as Transform;
+		maskTop.parent = mapHolder;
+		maskTop.localScale = new Vector3 (maxMapSize.x, 1, (maxMapSize.y - mapSize.y) / 2) * tileSize;
+
+		Transform maskBottom = Instantiate (navmeshMaskPrefab, Vector3.back * (mapSize.y + maxMapSize.y) / 4 * tileSize, Quaternion.identity) as Transform;
+		maskBottom.parent = mapHolder;
+		maskBottom.localScale = new Vector3 (maxMapSize.x, 1, (maxMapSize.y - mapSize.y) / 2) * tileSize;
+
 		navmeshFloor.localScale = new Vector3 (maxMapSize.x, maxMapSize.y) * tileSize;
 	}
 
