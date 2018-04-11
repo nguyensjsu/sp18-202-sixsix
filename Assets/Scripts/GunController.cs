83896f9fa84984944a9fe32913b2489a44a78e9f
Annan Wang <awang0523@gmail.com>
2018-04-11 02:46:07 -0700

    add fire mode and trail

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -1,29 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GunController : MonoBehaviour {
+public class GunController : MonoBehaviour
+{
 
-	public Transform weaponHold;
-	public Gun startingGun;
-	Gun equippedGun;
+    public Transform weaponHold;
+    public Gun startingGun;
+    Gun equippedGun;
 
-	void Start() {
-		if (startingGun != null) {
-			EquipGun(startingGun);
-		}
-	}
+    void Start()
+    {
+        if (startingGun != null)
+        {
+            EquipGun(startingGun);
+        }
+    }
 
-	public void EquipGun(Gun gunToEquip) {
-		if (equippedGun != null) {
-			Destroy(equippedGun.gameObject);
-		}
-		equippedGun = Instantiate (gunToEquip, weaponHold.position,weaponHold.rotation) as Gun;
-		equippedGun.transform.parent = weaponHold;
-	}
+    public void EquipGun(Gun gunToEquip)
+    {
+        if (equippedGun != null)
+        {
+            Destroy(equippedGun.gameObject);
+        }
+        equippedGun = Instantiate(gunToEquip, weaponHold.position, weaponHold.rotation) as Gun;
+        equippedGun.transform.parent = weaponHold;
+    }
 
-	public void Shoot() {
-		if (equippedGun != null) {
-			equippedGun.Shoot();
-		}
-	}
+    public void OnTriggerHold()
+    {
+        if (equippedGun != null)
+        {
+            equippedGun.OnTriggerHold();
+        }
+    }
+
+    public void OnTriggerRelease()
+    {
+        if (equippedGun != null)
+        {
+            equippedGun.OnTriggerRelease();
+        }
+    }
 }
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,49 +1,61 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Projectile : MonoBehaviour {
+public class Projectile : MonoBehaviour
+{
 
-	public LayerMask collisionMask;
-	float speed = 10;
-	float damage = 1;
+    public LayerMask collisionMask;
+    public Color trailColour;
+    float speed = 10;
+    float damage = 1;
 
-	float lifetime = 3;
-	float skinWidth = .1f;
+    float lifetime = 3;
+    float skinWidth = .1f;
 
-	void Start() {
-		Destroy (gameObject, lifetime);
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
 
-		Collider[] initialCollisions = Physics.OverlapSphere (transform.position, .1f, collisionMask);
-		if (initialCollisions.Length > 0) {
-			OnHitObject(initialCollisions[0], transform.position);
-		}
-	}
+        Collider[] initialCollisions = Physics.OverlapSphere(transform.position, .1f, collisionMask);
+        if (initialCollisions.Length > 0)
+        {
+            OnHitObject(initialCollisions[0], transform.position);
+        }
 
-	public void SetSpeed(float newSpeed) {
-		speed = newSpeed;
-	}
+        GetComponent<TrailRenderer>().material.SetColor("_TintColor", trailColour);
+    }
 
-	void Update () {
-		float moveDistance = speed * Time.deltaTime;
-		CheckCollisions (moveDistance);
-		transform.Translate (Vector3.forward * moveDistance);
-	}
+    public void SetSpeed(float newSpeed)
+    {
+        speed = newSpeed;
+    }
+
+    void Update()
+    {
+        float moveDistance = speed * Time.deltaTime;
+        CheckCollisions(moveDistance);
+        transform.Translate(Vector3.forward * moveDistance);
+    }
 
 
-	void CheckCollisions(float moveDistance) {
-		Ray ray = new Ray (transform.position, transform.forward);
-		RaycastHit hit;
+    void CheckCollisions(float moveDistance)
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
 
-		if (Physics.Raycast(ray, out hit, moveDistance + skinWidth, collisionMask, QueryTriggerInteraction.Collide)) {
-			OnHitObject(hit.collider, hit.point);
-		}
-	}
+        if (Physics.Raycast(ray, out hit, moveDistance + skinWidth, collisionMask, QueryTriggerInteraction.Collide))
+        {
+            OnHitObject(hit.collider, hit.point);
+        }
+    }
 
-	void OnHitObject(Collider c, Vector3 hitPoint) {
-		IDamageable damageableObject = c.GetComponent<IDamageable> ();
-		if (damageableObject != null) {
-			damageableObject.TakeHit(damage, hitPoint, transform.forward);
-		}
-		GameObject.Destroy (gameObject);
-	}
+    void OnHitObject(Collider c, Vector3 hitPoint)
+    {
+        IDamageable damageableObject = c.GetComponent<IDamageable>();
+        if (damageableObject != null)
+        {
+            damageableObject.TakeHit(damage, hitPoint, transform.forward);
+        }
+        GameObject.Destroy(gameObject);
+    }
 }
