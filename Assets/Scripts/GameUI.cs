03ec6d5d1b091a1cda282f6987cedd6aa84a3732
Tianxiang_Liu <34782522+MrMrEcho@users.noreply.github.com>
2018-04-14 14:25:41 -0700

    Add sliding UI. Need to keep testing proper parameters.

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -2,33 +2,91 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class GameUI : MonoBehaviour {
+public class GameUI : MonoBehaviour
+{
 
-	public Image fadePlane;
-	public GameObject gameOverUI;
+    public Image fadePlane;
+    public GameObject gameOverUI;
 
-	void Start () {
-		FindObjectOfType<Player> ().OnDeath += OnGameOver;
-	}
+    public RectTransform newWaveBanner;
+    public Text newWaveTitle;
+    public Text newWaveEnemyCount;
 
-	void OnGameOver() {
-		StartCoroutine(Fade (Color.clear, Color.black,1));
-		gameOverUI.SetActive (true);
-	}
+    Spawner spawner;
 
-	IEnumerator Fade(Color from, Color to, float time) {
-		float speed = 1 / time;
-		float percent = 0;
+    void Start()
+    {
+        FindObjectOfType<Player>().OnDeath += OnGameOver;
+    }
 
-		while (percent < 1) {
-			percent += Time.deltaTime * speed;
-			fadePlane.color = Color.Lerp(from,to,percent);
-			yield return null;
-		}
-	}
+    void Awake()
+    {
+        spawner = FindObjectOfType<Spawner>();
+        spawner.OnNewWave += OnNewWave;
+    }
 
-	// UI Input
-	public void StartNewGame() {
-		Application.LoadLevel ("Game");
-	}
+    void OnNewWave(int waveNumber)
+    {
+        string[] numbers = { "One", "Two", "Three", "Four", "Five" };
+        newWaveTitle.text = "- Wave " + numbers[waveNumber - 1] + " -";
+        string enemyCountString = ((spawner.waves[waveNumber - 1].infinite) ? "Infinite" : spawner.waves[waveNumber - 1].enemyCount + "");
+        newWaveEnemyCount.text = "Enemies: " + enemyCountString;
+
+        StopCoroutine("AnimateNewWaveBanner");
+        StartCoroutine("AnimateNewWaveBanner");
+    }
+
+    void OnGameOver()
+    {
+        StartCoroutine(Fade(Color.clear, Color.black, 1));
+        gameOverUI.SetActive(true);
+    }
+
+    IEnumerator AnimateNewWaveBanner()
+    {
+
+        float delayTime = 1.5f;
+        float speed = 3f;
+        float animatePercent = 0;
+        int dir = 1;
+
+        float endDelayTime = Time.time + 1 / speed + delayTime;
+
+        while (animatePercent >= 0)
+        {
+            animatePercent += Time.deltaTime * speed * dir;
+
+            if (animatePercent >= 1)
+            {
+                animatePercent = 1;
+                if (Time.time > endDelayTime)
+                {
+                    dir = -1;
+                }
+            }
+
+            newWaveBanner.anchoredPosition = Vector2.up * Mathf.Lerp(-225, 5, animatePercent);
+            yield return null;
+        }
+
+    }
+
+    IEnumerator Fade(Color from, Color to, float time)
+    {
+        float speed = 1 / time;
+        float percent = 0;
+
+        while (percent < 1)
+        {
+            percent += Time.deltaTime * speed;
+            fadePlane.color = Color.Lerp(from, to, percent);
+            yield return null;
+        }
+    }
+
+    // UI Input
+    public void StartNewGame()
+    {
+        Application.LoadLevel("Game");
+    }
 }
