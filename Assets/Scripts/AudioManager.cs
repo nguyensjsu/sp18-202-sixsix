6e6435113b494153a55d64bc83fdc1ad1fa27bae
Annan Wang <awang0523@gmail.com>
2018-04-15 12:53:35 -0700

    add audio manager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    float masterVolumePercent = 1;
+    float sfxVolumePercent = 1;
+    float musicVolumePercent = 1;
+
+    AudioSource[] musicSources;
+
+    void Awake()
+    {
+        musicSources = new AudioSource[2];
+        for (int i = 0; i < 2; i++)
+        {
+            GameObject newMusicSource = new GameObject("Music source " + (i + 1));
+            musicSources[i] = newMusicSource.AddComponent<AudioSource>();
+            newMusicSource.transform.parent = transform;
+        }
+
+    }
+
+    public void PlaySound(AudioClip clip, Vector3 pos)
+    {
+        if (clip != null)
+        {
+            AudioSource.PlayClipAtPoint(clip, pos, sfxVolumePercent * masterVolumePercent);
+        }
+    }
+
+
+}
