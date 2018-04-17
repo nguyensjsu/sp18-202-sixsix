15133e44dcaed43b1e6929027ae534b15daceb31
WenqiWang <ww82@sjsu.edu>
2018-04-17 01:12:19 -0700

    adjust start scene to be menu scene

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,8 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour {
 
+	public GameObject mainMenuHolder;
+	public GameObject optionsMenuHolder;
 
+	public void Play() {
+		SceneManager.LoadScene ("Game");
+	}
+
+	public void Quit() {
+		Application.Quit ();
+	}
+
+	public void OptionsMenu() {
+		mainMenuHolder.SetActive (false);
+		optionsMenuHolder.SetActive (true);
+	}
+
+	public void MainMenu() {
+		optionsMenuHolder.SetActive (false);
+		mainMenuHolder.SetActive (true);
+	}
+
+	public void SetScreenResolution(int i) {
+
+	}
+
+	public void SetFullscreen(bool isFullScreen) {
+
+	}
+
+	public void SetMasterVolume(float value) {
+		
+	}
+
+	public void SetMusicVolume(float value) {
+
+	}
+
+	public void SetSfxVolume(float value) {
+
+	}
 }
