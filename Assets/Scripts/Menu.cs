1af400f1bf86bdcb5711cd230c3a62e2ca7a04e8
WenqiWang <ww82@sjsu.edu>
2018-04-17 01:42:39 -0700

    implement screen res toggle functions

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour {
 
 	public GameObject mainMenuHolder;
 	public GameObject optionsMenuHolder;
+
+	public Slider[] volumeSliders;
+	public Toggle[] resolutionToggles;
+	public int[] screenWidths;
+	int activeScreenResIndex;
 
 	public void Play() {
 		SceneManager.LoadScene ("Game");
@@ -27,11 +33,24 @@ public class Menu : MonoBehaviour {
 	}
 
 	public void SetScreenResolution(int i) {
-
+		if (resolutionToggles [i].isOn) {
+			activeScreenResIndex = i;
+			float aspectRatio = 16 / 9f;
+			Screen.SetResolution (screenWidths [i], (int)(screenWidths[i]/aspectRatio), false);
+		}
 	}
 
 	public void SetFullscreen(bool isFullScreen) {
-
+		for (int i = 0; i < resolutionToggles.Length; i++) {
+			resolutionToggles [i].interactable = !isFullScreen;
+		}
+		if (isFullScreen) {
+			Resolution[] allResolutions = Screen.resolutions;
+			Resolution maxResolution = allResolutions [allResolutions.Length - 1];
+			Screen.SetResolution (maxResolution.width, maxResolution.height, true);
+		} else {
+			SetScreenResolution (activeScreenResIndex);
+		}
 	}
 
 	public void SetMasterVolume(float value) {
