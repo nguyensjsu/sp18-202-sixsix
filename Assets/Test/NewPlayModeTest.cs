a410293317835c57dbe3aaa594ca78a20d82ec5d
na-yue <na.yue@sjsu.edu>
2018-04-23 16:59:37 -0700

    add unit test case

---
+++ Assets/Test/NewPlayModeTest.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class NewPlayModeTest {
+
+	[Test]
+	public void NewPlayModeTestSimplePasses() {
+		// Use the Assert class to test conditions.
+	}
+
+	// A UnityTest behaves like a coroutine in PlayMode
+	// and allows you to yield null to skip a frame in EditMode
+	[UnityTest]
+	public IEnumerator NewPlayModeTestWithEnumeratorPasses() {
+		// Use the Assert class to test conditions.
+		// yield to skip a frame
+		yield return null;
+	}
+
+	[UnityTest]
+	public IEnumerator GameObject_WithRigidBody_WillBeAffectedByPhysics()
+	{
+		var go = new GameObject();
+		go.AddComponent<Rigidbody>();
+		var originalPosition = go.transform.position.y;
+
+		yield return new WaitForFixedUpdate();
+
+		Assert.AreNotEqual(originalPosition, go.transform.position.y);
+	}
+}
