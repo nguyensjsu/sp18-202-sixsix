7c656c4e3180f272bc37f15286195ee371e11676
na-yue <na.yue@sjsu.edu>
2018-04-28 16:06:02 -0700

    modified class diagram

--- Assets/Test/NewPlayModeTest.cs
+++ Assets/Test/NewPlayModeTest.cs
@@ -14,9 +14,12 @@ public class NewPlayModeTest {
 	// and allows you to yield null to skip a frame in EditMode
 	[UnityTest]
 	public IEnumerator NewPlayModeTestWithEnumeratorPasses() {
-		// Use the Assert class to test conditions.
-		// yield to skip a frame
-		yield return null;
+		SetupScene ();
+		var menuHolder = new GameObject ().AddComponent<mainMenuHolder> ();
+	}
+
+	void SetupScene(){
+		
 	}
 
 	[UnityTest]
